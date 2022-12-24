7f62bf6b17583fa0256ff4e634a5df69695077df
Zp3sss <zp3sss@163.com>
2022-12-24 19:33:43 +0800

    测试amend

--- master/demo1.java
+++ master/demo1.java
@@ -2,6 +2,10 @@ package master;
 
 public class demo1 {
     public static void func1() {
-        System.out.println("jajaja");
+        System.out.println("demo1方法");
+    }
+
+    public static void func2() {
+        System.out.println("master: func2");
     }
 }

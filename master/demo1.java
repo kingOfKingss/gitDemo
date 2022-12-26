4171c7660f10337f899dad43c5c91b00e7c30ef3
Zp3sss <zp3sss@163.com>
2022-12-26 23:16:48 +0800

    gitdemo1 -- demo1 -- 第二次提交

--- master/demo1.java
+++ master/demo1.java
@@ -4,5 +4,6 @@ public class demo1 {
     public static void func1() {
         System.out.println("java - demo1 - func1");
         System.out.println("gitDemo1");
+        System.out.println("gitDemo1 - 第二次更新");
     }
 }

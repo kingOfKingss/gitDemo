c3571029ec5e0235ffe1516e4e07d8532ee0bafe
Zp3sss <zp3sss@163.com>
2023-01-08 13:41:35 +0800

    dev: demo3 -3

--- master/Demo3.java
+++ master/Demo3.java
@@ -4,5 +4,6 @@ public class Demo3 {
     public void func1(){
         System.out.println("dev: demo3-func1 - 1");
         System.out.println("dev: demo3-func1 - 2");
+        System.out.println("dev: demo3-func1 - 3");
     }
 }

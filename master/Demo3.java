5addfa002c2e203a1f8b4d9467c306a4c8038f70
Zp3sss <zp3sss@163.com>
2023-01-08 13:42:17 +0800

    Revert "撤销修改: dev: demo3 -3"
    This reverts commit c3571029ec5e0235ffe1516e4e07d8532ee0bafe.

--- master/Demo3.java
+++ master/Demo3.java
@@ -4,6 +4,5 @@ public class Demo3 {
     public void func1(){
         System.out.println("dev: demo3-func1 - 1");
         System.out.println("dev: demo3-func1 - 2");
-        System.out.println("dev: demo3-func1 - 3");
     }
 }
